adb83f56764f089761b0782fa28e6874480581e5
Jonathan Lucas <jalucas@gmx.com>
2018-11-18 22:02:47 -0600

    Added "port decl to port assign" commands.

--- vhdl_menu.py
+++ vhdl_menu.py
@@ -20,3 +20,43 @@ class VhdlPortDeclToSignalDeclCommand(sublime_plugin.TextCommand):
 			
 			self.view.replace(edit, region, "\n".join(lines_new));
 			#print("\n".join(lines_new)); 
+
+
+class VhdlPortDeclToPortAssignCopyCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		selection = self.view.sel()
+		for region in selection:
+			region_text = self.view.substr(region)
+			lines_new = [];
+			lines_orig = region_text.split("\n");
+			for line in lines_orig:
+				# Align comment lines
+				line = re.sub(r'^\s*(--.*)$', r'  \1', line, flags=re.IGNORECASE);
+				# Convert the ports to signals.
+				# FIXME: Insert semicolon always, even if port doesn't have one.
+				line = re.sub(r'^\s*([a-z0-9_]+\s*):\s*(?:IN|OUT|INOUT|BUFFER)\s+(.+)$', r'  \1=> \1,', line, flags=re.IGNORECASE);
+
+				lines_new.append(line);
+			
+			self.view.replace(edit, region, "\n".join(lines_new));
+			#print("\n".join(lines_new)); 
+
+
+class VhdlPortDeclToPortAssignBlankCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		selection = self.view.sel()
+		for region in selection:
+			region_text = self.view.substr(region)
+			lines_new = [];
+			lines_orig = region_text.split("\n");
+			for line in lines_orig:
+				# Align comment lines
+				line = re.sub(r'^\s*(--.*)$', r'  \1', line, flags=re.IGNORECASE);
+				# Convert the ports to signals.
+				# FIXME: Insert semicolon always, even if port doesn't have one.
+				line = re.sub(r'^\s*([a-z0-9_]+\s*):\s*(?:IN|OUT|INOUT|BUFFER)\s+(.+)$', r'  \1=> ,', line, flags=re.IGNORECASE);
+
+				lines_new.append(line);
+			
+			self.view.replace(edit, region, "\n".join(lines_new));
+			#print("\n".join(lines_new)); 
